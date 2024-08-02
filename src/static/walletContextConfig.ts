15271674a583aeabab0bcadf5c727dd330d2e135
astro <astromanrx@gmail.com>
2024-08-02 09:30:22 +0330

    Walelt connect id added.

--- src/static/walletContextConfig.ts
+++ src/static/walletContextConfig.ts
@@ -2,7 +2,7 @@ import { http, createConfig } from "wagmi";
 import { base, mainnet } from "wagmi/chains";
 import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
 
-const projectId = "<WALLETCONNECT_PROJECT_ID>";
+const projectId = "8a79024c3660b69f6c334be0723c0eeb";
 
 export const config = createConfig({
   chains: [mainnet, base],
